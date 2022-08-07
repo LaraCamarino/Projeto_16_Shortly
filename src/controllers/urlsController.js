240bb2e0e2a345101f95cfb5ab5161adc6cd9204
LaraCamarino <laracamarino@gmail.com>
2022-08-06 23:22:35 -0300

    adds GetUrlById route

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -6,11 +6,35 @@ export async function shortenUrl(req, res) {
     const { url } = req.body;
     const { userId } = res.locals.verifyValidToken;
     const shortUrl = nanoid(8);
-   
+
     try {
         await connection.query(`INSERT INTO urls (url, "shortUrl", "creatorId") VALUES ($1, $2, $3)`, [url, shortUrl, userId]);
 
-        res.status(201).send({shortUrl});
+        res.status(201).send({ shortUrl });
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export async function getUrlById(req, res) {
+    const { id } = req.params;
+
+    try {
+        const result = await connection.query("SELECT * FROM urls WHERE id = $1", [id]);
+
+        if (result.rowCount === 0) {
+            res.status(404).send("There is no URL with that ID.");
+            return;
+        }
+
+        const url = result.rows[0];
+
+        delete url.visitCount;
+        delete url.creatorId;
+        delete url.createdAt;
+
+        res.status(200).send(url);
     }
     catch (error) {
         res.status(500).send(error);
--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/urlsController.js";
+import { shortenUrl, getUrlById } from "../controllers/urlsController.js";
 import validateToken from "../middlewares/validateToken.js";
 import validateUrl from "../middlewares/validateUrl.js";
 
 const router = Router();
 
 router.post("/urls/shorten", validateToken, validateUrl, shortenUrl);
+router.get("/urls/:id", getUrlById);
 
 export default router;
