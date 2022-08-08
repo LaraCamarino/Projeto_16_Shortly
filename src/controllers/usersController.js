59ef2428d755c59a34e8530ac4ee4a5e2dee5d60
LaraCamarino <laracamarino@gmail.com>
2022-08-08 00:47:41 -0300

    adds GetRanking route

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -13,8 +13,8 @@ export async function getUserInfo(req, res) {
 
         const visitCount = await connection.query(`SELECT SUM("visitCount") FROM urls WHERE "creatorId" = $1`, [userId]);
 
-        const { rows: arrayUrls} = await connection.query(`SELECT id, url, "shortUrl", "visitCount" FROM urls WHERE "creatorId" = $1`, [userId]);
-        
+        const { rows: arrayUrls } = await connection.query(`SELECT id, url, "shortUrl", "visitCount" FROM urls WHERE "creatorId" = $1`, [userId]);
+
         const formattedResult = {
             "id": verifyExistingUser.rows[0].id,
             "name": verifyExistingUser.rows[0].name,
@@ -23,6 +23,22 @@ export async function getUserInfo(req, res) {
         }
 
         res.status(200).send(formattedResult);
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export async function getRanking(req, res) {
+    try {
+        const { rows: ranking } = await connection.query(`SELECT users.id, users.name, COUNT(url) AS "linksCount", COALESCE(SUM(urls."visitCount"),0) as "visitCount"
+        FROM users 
+        LEFT JOIN urls 
+        ON urls."creatorId" = users.id 
+        GROUP BY users.id
+        ORDER BY "visitCount" DESC
+        LIMIT 10`);
+        res.status(200).send(ranking);
     }
     catch (error) {
         res.status(500).send(error);
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUserInfo } from "../controllers/usersController.js"
+import { getUserInfo, getRanking } from "../controllers/usersController.js"
 import validateToken from "../middlewares/validateToken.js"
 
 const router = Router();
 
 router.get("/users/me", validateToken, getUserInfo);
+router.get("/ranking", getRanking)
 
 export default router;
